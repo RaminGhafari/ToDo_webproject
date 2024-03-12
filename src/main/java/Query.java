aa188d2cf539664b4f21a58860f348fded2a7f34
RaminGhafari <rg.raminghafari@yahoo.com>
2024-03-12 01:59:00 -0500

    Add show.java servlet to show all tasks stored in the database

--- src/main/java/Query.java
+++ src/main/java/Query.java
@@ -1,4 +1,9 @@
 
 public class Query {
 	static String insert = "INSERT INTO ToDoTable (id, TITLE, DATE, PRIORITY, CATEGORY, STATUS) VALUES (?, ?, ?, ?, ?, ?)";
+	static String show = "SELECT * FROM ToDoTable";
+	static String update = "UPDATE ToDoTable SET STATUS=? WHERE id=?";
+	static String updateShow = "SELECT * FROM ToDoTable WHERE id=?";
+	static String delete = "DELETE FROM ToDoTable where id=?";
+	static String deleteShow = "SELECT * FROM ToDoTable WHERE id<>?";
 }
